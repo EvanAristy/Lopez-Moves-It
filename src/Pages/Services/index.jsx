dc9d7af8f7dee320a551818334351892b37a5bc2
EvanAristy <evanjaristy@gmail.com>
2022-04-11 18:43:55 -0400

    started the services page

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -3,7 +3,54 @@ import React from 'react';
 const Services = () => {
     return (
         <div>
-            
+            <h1>What Can We Do For You?</h1>
+
+            <div className="ui card">
+                <a className="image" href="#">
+                    <img></img>
+                </a>
+                <div className="content">
+                    <p className="header">Junk Removal</p>
+                </div>
+            </div>
+
+            <div className="ui card">
+                <a className="image" href="#">
+                    <img></img>
+                </a>
+                <div className="content">
+                    <p className="header">Item Relocation</p>
+                </div>
+            </div>
+
+            <div className="ui card">
+                <a className="image" href="#">
+                    <img></img>
+                </a>
+                <div className="content">
+                    <p className="header">Item Pick-up/Drop off</p>
+                </div>
+            </div>
+
+            <div className="ui card">
+                <a className="image" href="#">
+                    <img></img>
+                </a>
+                <div className="content">
+                    <p className="header">Moving</p>
+                </div>
+            </div>
+
+            <div className="ui card">
+                <a className="image" href="#">
+                    <img></img>
+                </a>
+                <div className="content">
+                    <p className="header">Automotive Parts</p>
+                </div>
+            </div>
+
+
         </div>
     );
 }
