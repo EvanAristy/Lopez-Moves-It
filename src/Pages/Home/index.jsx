2d7d7b94a710c72fe948b9906cea19b94e5c15ad
EvanAristy <evanjaristy@gmail.com>
2022-04-13 13:48:33 -0400

    added a carousel for testimaonials and cards for serices. waiting for pictures to plug everything in

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,7 +3,41 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
+
             <h1>hello world</h1>
+            
+            <section id="intro">
+                <h1>Need Junk Removal? Furniture Moved? Give Us A Call</h1>
+            </section>
+
+            <section id="testimonials">
+                <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src="..." className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="..." className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="..." className="d-block w-100" alt="..." />
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </section>
+
+            <footer>
+                
+            </footer>
+
         </div>
     );
 }
